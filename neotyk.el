65d1f4f18e5849720e1833cbd7034a2fabe11dee
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-03-14 10:31:28 +0100

    A bit bigger font.

--- neotyk.el
+++ neotyk.el
@@ -91,7 +91,7 @@
 
 ;; Inconsolata font
 (set-face-attribute 'default nil
-                    :family "Inconsolata" :height 130 :weight 'normal)
+                    :family "Inconsolata" :height 140 :weight 'normal)
 
 (set-face-font 'default "-apple-Inconsolata-medium-normal-normal-*-*-*-*-*-m-0-iso10646-1")
 (set-face-font 'org-column "-apple-Inconsolata-medium-normal-normal-*-*-*-*-*-m-0-iso10646-1")

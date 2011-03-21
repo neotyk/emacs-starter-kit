bbe271990b44c9e1a1a65b22e3dd836ae9009cd7
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-03-21 08:52:17 +0100

    Added yasnippet, and simple clojure template file.

--- neotyk.el
+++ neotyk.el
@@ -127,3 +127,10 @@
 
 ;; desktop save mode
 (setq desktop-save-mode 1)
+
+;; yasnippet
+(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet/")
+(require 'yasnippet)
+(yas/initialize)
+(setq yas/snippet-dirs '("~/.emacs.d/snippets"
+                         "~/.emacs.d/plugins/yasnippet/snippets"))

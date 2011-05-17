f29f9ab0c8c5a4fcc9c9acfde6d5eb08d1b41d0a
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-05-17 09:27:07 +0200

    usability topic

--- neotyk.el
+++ neotyk.el
@@ -4,49 +4,14 @@
 (require 'clojure-topic)
 (require 'org-topic)
 
-;; ui topic
 (require 'ui-topic)
-
-(setq flyspell-mode t)
+(require 'ux-topic)
 
 ;(setq next-line-add-newlines t)
-
-(require 'recentf)
- 
-;; get rid of `find-file-read-only' and replace it with something
-;; more useful.
-(global-set-key (kbd "C-x C-r") 'ido-recentf-open)
- 
-;; enable recent files mode.
-(recentf-mode t)
- 
-; 50 files ought to be enough.
-(setq recentf-max-saved-items 50)
- 
-(defun ido-recentf-open ()
-  "Use `ido-completing-read' to \\[find-file] a recent file"
-  (interactive)
-  (let ((home (expand-file-name (getenv "HOME"))))
-    (find-file
-     (ido-completing-read "Recentf open: "
-                          (mapcar (lambda (path)
-                                    (replace-regexp-in-string home "~" path))
-                                  recentf-list)
-                          nil t))))
 
 ;; closure-template
 (add-to-list 'load-path "~/.emacs.d/cl-closure-template/")
 (require 'closure-template-html-mode)
-
-;; desktop save mode
-(setq desktop-save-mode 1)
-
-;; yasnippet
-(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet/")
-(require 'yasnippet)
-(yas/initialize)
-(setq yas/snippet-dirs '("~/.emacs.d/snippets"
-                         "~/.emacs.d/plugins/yasnippet/snippets"))
 
 ;; align configuration
 (require 'align)

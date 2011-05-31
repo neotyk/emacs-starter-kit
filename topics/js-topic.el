36edd960523bda0e28433da72bba477fe723daee
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-05-31 23:22:46 +0200

    js2-mode added

--- topics/js-topic.el
+++ topics/js-topic.el
@@ -2,4 +2,8 @@
 (add-to-list 'load-path "~/.emacs.d/cl-closure-template/")
 (require 'closure-template-html-mode)
 
+(add-to-list 'load-path "~/.emacs.d/js2-mode/")
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
 (provide 'js-topic)

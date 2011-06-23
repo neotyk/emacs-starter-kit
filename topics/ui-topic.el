7735cad6aeff6da022bec83f57533ff98baefcb6
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-06-23 10:40:48 +0200

    compiled from https://github.com/typester/emacs so I can has fullscreen now.

--- topics/ui-topic.el
+++ topics/ui-topic.el
@@ -5,6 +5,9 @@
 (set-face-font 'default "-apple-Inconsolata-medium-normal-normal-*-*-*-*-*-m-0-iso10646-1")
 (set-face-font 'org-column "-apple-Inconsolata-medium-normal-normal-*-*-*-*-*-m-0-iso10646-1")
 
+;; fullscreen
+(global-set-key (kbd "M-RET") 'ns-toggle-fullscreen)
+
 (require 'custom-erc-topic)
 (require 'custom-growl-topic)
 

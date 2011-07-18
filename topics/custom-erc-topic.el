ea80863a40f5e32a1068d1e98f899655c1222a7d
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-07-18 08:01:51 +0200

    white was not visible

--- topics/custom-erc-topic.el
+++ topics/custom-erc-topic.el
@@ -7,7 +7,7 @@
 ;; Define the list of colors to use when coloring IRC nicks
 (setq-default erc-colors-list '("blue" "green" "yellow"
                                 "gray" "brown" "red"
-                                "purple" "white" "cyan"))
+                                "purple" "black" "cyan"))
 
 (defun build-nick-face-list ()
   "build-nick-face-list builds a list of new faces using the
